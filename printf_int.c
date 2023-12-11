f4706f83e70d9c090889970eaf75c3947163a9fc
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-11 20:31:11 +0200

    Update printf_int.c

--- printf_int.c
+++ printf_int.c
@@ -1,15 +1,13 @@
 #include "holberton.h"
-
 /**
- * printf_char - prints a char.
- * @val: arguments.
- * Return: 1.
- */
+* printf_char - prints a char.
+* @val: arguments.
+* Return: 1.
+*/
 int printf_char(va_list val)
 {
-	char s;
-
-	s = va_arg(val, int);
-	_putchar(s);
-	return (1);
+char s;
+s = va_arg(val, int);
+putchar(s);
+return (1);
 }
